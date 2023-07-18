1ae387afb45bc3eebabbcf56b376889afe25eb19
Douglas Loureiro <douglas.loureiro3@gmail.com>
2023-07-18 19:29:39 -0300

    Manipulating DataFrames in the real world
    Filtering column content with Python

--- Cleaning Data with PySpark/cleaning-data-with-pyspark.py
+++ Cleaning Data with PySpark/cleaning-data-with-pyspark.py
@@ -54,6 +54,19 @@ avg_duration_df = spark.sql('SELECT avg(flight_duration) from flights').show()
 
 ## Manipulating DataFrames in the real world
 
+# Show the distinct VOTER_NAME entries
+voter_df.select('VOTER_NAME').distinct().show(40, truncate=False)
+
+# Filter voter_df where the VOTER_NAME is 1-20 characters in length
+voter_df = voter_df.filter('length(VOTER_NAME) > 0 and length(VOTER_NAME) < 20')
+
+# Filter out voter_df where the VOTER_NAME contains an underscore
+voter_df = voter_df.filter(~ F.col('VOTER_NAME').contains('_'))
+
+# Show the distinct VOTER_NAME entries again
+voter_df.select('VOTER_NAME').distinct().show(40, truncate=False)
+
+
 ## Improving Performance
 
 ## Improving Performance
