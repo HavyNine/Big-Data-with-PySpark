fa3a1e46ccbdb2e68d62c427b2e2895c8a34575b
Douglas Loureiro <douglas.loureiro3@gmail.com>
2023-07-23 14:54:28 -0300

    Further parsing

--- Cleaning Data with PySpark/cleaning-data-with-pyspark.py
+++ Cleaning Data with PySpark/cleaning-data-with-pyspark.py
@@ -299,4 +299,18 @@ split_df = split_df.withColumn('height',  split_cols.getItem(3))
 # Add split_cols as a column
 split_df = split_df.withColumn('split_cols', split_cols)
 
-# 
+## Futher parsing - 22
+
+def retriever(cols, colcount):
+  # Return a list of dog data
+  return cols[4:colcount]
+
+# Define the method as a UDF
+udfRetriever = F.udf(retriever, ArrayType(StringType()))
+
+split_df.show()
+# Create a new column using your UDF
+split_df = split_df.withColumn('dog_list', udfRetriever(split_df.split_cols,split_df.colcount))
+
+# Remove the original column, split_cols, and the colcount
+split_df = split_df.drop('split_cols').drop('_c0').drop('colcount')
